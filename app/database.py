85370e24111fccc8bbe07f79fc091c46645e5c85
unknown <nrmanranoa@yahoo.com>
2024-02-16 21:38:51 +0100

    Added instructions

--- app/database.py
+++ app/database.py
@@ -3,7 +3,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:tRIPHOSPHATE1@localhost/fastapi'
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
